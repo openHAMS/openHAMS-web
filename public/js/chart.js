425e1d32c24ad03461fe7bb51f96a908a60b5565
László Székely-Tóth <notisracer@gmail.com>
2017-04-22 22:30:22 +0200

    Implemented async & scaling graph loading on zooming and/or moving

--- public/js/chart.js
+++ public/js/chart.js
@@ -6,24 +6,48 @@ Highcharts.setOptions({
     }
 });
 
+var stillUpdating = false;
+
+function addFirst(data) {
+    data.splice(0, 0, [1492300800000, null]);
+    return data;
+}
+
 function afterSetExtremes(e) {
-    console.log(e.min, e.max);
+    if (!stillUpdating) {
+        var currExMin = (Math.round(e.min / 1000) * 1000);
+        var currExMax = (Math.round(e.max / 100) * 100);
+        stillUpdating = true;
+        chart.showLoading('Loading data from server...');
+        $.getJSON('jsonp?start=' + currExMin +
+            '&end=' + currExMax + '&callback=?',
+            function(data) {
+                chart.series[0].setData(addFirst(data["pressure"]));
+                chart.series[1].setData(addFirst(data["temperature"]));
+                chart.hideLoading();
+                stillUpdating = false;
+            });
+    }
 }
 
 function loadGraph(history) {
     var seriesOptions = [];
     seriesOptions[0] = {
         color: Highcharts.getOptions().colors[1],
-        data: history["pressure"],
+        data: addFirst(history["pressure"]),
         name: 'Pressure',
-        tooltip: { valueSuffix: ' hPa' },
+        tooltip: {
+            valueSuffix: ' hPa'
+        },
         yAxis: 0
     };
     seriesOptions[1] = {
         color: Highcharts.getOptions().colors[0],
-        data: history["temperature"],
+        data: addFirst(history["temperature"]),
         name: 'Temperature',
-        tooltip: { valueSuffix: ' ℃' },
+        tooltip: {
+            valueSuffix: ' ℃'
+        },
         yAxis: 1
     };
     chartSettings.series = seriesOptions;
--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var subscribeList = [
 ];
 
 
-mqtt.on('connect', function () {
+mqtt.on('connect', function() {
     console.log('mqtt conn');
     subscribeList.forEach(s => {
         console.log(s);
@@ -31,27 +31,63 @@ mqtt.on('connect', function () {
     });
 });
 
-mqtt.on('message', function (topic, message) {
+mqtt.on('message', function(topic, message) {
     io.sockets.emit(topic, message.toString());
 });
 
 function filterMeasurements(names) {
     var availableMeasurements = subscribeList.map(mqttAddr => {
-            var mqttSplit = mqttAddr.split('/');
-            return mqttSplit[mqttSplit.length - 1];
-        }).filter(queryMeasurement => {
-            return names.includes(queryMeasurement);
-        });
+        var mqttSplit = mqttAddr.split('/');
+        return mqttSplit[mqttSplit.length - 1];
+    }).filter(queryMeasurement => {
+        return names.includes(queryMeasurement);
+    });
     return availableMeasurements;
 }
 
 function measToQ(availableMeasurement, start, end) {
     // return 'SELECT value FROM ' + availableMeasurement + ' WHERE time > now() - 3h';
-    return 'SELECT MEAN(value) AS value FROM ' + availableMeasurement + ' WHERE time > now() - 3h GROUP BY time(5m) fill(none)'
+    if (start == null || end == null) {
+        return 'SELECT MEAN(value) AS value FROM ' +
+            availableMeasurement +
+            ' WHERE time > now() - 6h GROUP BY time(5m) fill(none)';
+    } else {
+        var duration = end - start;
+        start = String(start + "0000000000000000000").substring(0, 19);
+        end = String(end + "0000000000000000000").substring(0, 19);
+        if (duration < 30 * 60 * 1000) {
+            // 1m
+            return 'SELECT value FROM ' + availableMeasurement + ' WHERE time > now() - 3h';
+        } else {
+            var q = 'SELECT MEAN(value) AS value FROM ' + availableMeasurement +
+                ' WHERE time > ' + start + ' AND time < ' + end;
+            if (duration < 2 * 60 * 60 * 1000) {
+                // 1h - 1.25m
+                q += ' GROUP BY time(75s)';
+            } else if (duration < 4.5 * 60 * 60 * 1000) {
+                // 3h - 3.75m
+                q += ' GROUP BY time(225s)';
+            } else if (duration < 9 * 60 * 60 * 1000) {
+                // 6h - 7.5m
+                q += ' GROUP BY time(450s)';
+            } else if (duration < 18 * 60 * 60 * 1000) {
+                // 12h - 15m
+                q += ' GROUP BY time(900s)';
+            } else {
+                // 24h - 30m
+                q += ' GROUP BY time(1800s)';
+            }
+            q += " fill(none)";
+            console.log('q: ', q);
+            return q;
+        }
+    }
 }
 
 function makeQueries(measurements, start, end) {
-    return Promise.all([measurements, measurements.map(m => { return measToQ(m, start, end); })]);
+    return Promise.all([measurements, measurements.map(m => {
+        return measToQ(m, start, end);
+    })]);
 }
 
 function queryInflux(measurements, queries) {
@@ -65,8 +101,8 @@ function transformInfluxData(results, measurements) {
     var data = {};
     for (var i = 0; i < results.length; i++) {
         data[measurements[i]] = results[i].map(result => {
-                return [parseInt(result.time.getNanoTime().slice(0, 13)), parseFloat(Math.round(result.value * 100) / 100)];
-            });
+            return [parseInt(result.time.getNanoTime().slice(0, 13)), parseFloat(Math.round(result.value * 100) / 100)];
+        });
     }
     return data;
 }
@@ -90,30 +126,33 @@ function getInfluxData(start, end) {
 
 }
 
-io.on('connection', function (socket) {
-    var start = 0;
-    var end = 0;
-    getInfluxData().then(data => { io.emit('server/history', data); });
+io.on('connection', function(socket) {
+    getInfluxData().then(data => {
+        io.emit('server/history', data);
+    });
 });
 
 app.use(express.static('public'));
 app.use('/static', express.static('public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/json', function (req, res) {
-    var start = 0;
-    var end = 0;
-    getInfluxData().then(data => { res.json(data); });
+app.get('/jsonp', function(req, res) {
+    console.log('jsonp');
+    var start = req.query.start;
+    var end = req.query.end;
+    getInfluxData(start, end).then(data => {
+        res.jsonp(data);
+    });
 });
 
 
 var PING_URL = "http://ipecho.net/plain";
 var OWN_URL = "http://racerzeroone.duckdns.org/";
-http.get(PING_URL, function (res) {
-    res.on("data", function (data) {
+http.get(PING_URL, function(res) {
+    res.on("data", function(data) {
         console.log("================================================================================");
         console.log("    ip:  " + data + ":8081");
         console.log("    url: " + OWN_URL);
@@ -122,6 +161,6 @@ http.get(PING_URL, function (res) {
 });
 
 
-server.listen(8081, function () {
+server.listen(8081, function() {
     console.log('listening');
 });
