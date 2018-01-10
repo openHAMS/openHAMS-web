fa9d27fd260ccaa8ec2bd5ae80db69ae99f65952
László Székely-Tóth <notisracer@gmail.com>
2018-01-10 19:35:12 +0100

    Implemented config microservice

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const favicon = require('serve-favicon');
 const http = require('http');
 const pug = require('pug');
 
-const jsonfile = require('jsonfile')
+const syncRequest = require('sync-request');
 const server = http.Server(app);
 const io = require('socket.io')(server);
 
@@ -20,16 +20,14 @@ const mqtt = Mqtt.connect('mqtt://127.0.0.1:1883');
 // connecting to InfluxDB
 const DB = require('./db.js');
 const db = new DB('127.0.0.1', 'sensors');
-
-// loading config & MQTT subscribes
-const config = jsonfile.readFileSync('config.json', 'utf8');
-const subscribes = config.subscribes
-	.filter((v, i, a) => a.indexOf(v) === i);
+// loading config & MQTT channels
+const config = JSON.parse(syncRequest('GET', 'http://localhost:8000/cards').getBody('utf8'));
+const channels = JSON.parse(syncRequest('GET', 'http://localhost:8000/channels').getBody('utf8'));
 
 
 mqtt.on('connect', function() {
     console.log('mqtt conn:');
-    subscribes.forEach(s => {
+    channels.forEach(s => {
         console.log(`    ${s}`);
         mqtt.subscribe(s);
     });
