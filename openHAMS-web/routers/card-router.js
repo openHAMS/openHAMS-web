0e5cea91134d763b8ca7c2c6480f693b484357c6
László Székely-Tóth <notisracer@gmail.com>
2018-03-08 22:03:33 +0100

    Renamed folders & made every microservice independent

---
+++ openHAMS-web/routers/card-router.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const express = require('express');
+const router = express.Router();
+
+const rp = require('request-promise-native');
+
+const configUrl = 'http://localhost:8000';
+const dataUrl = 'http://localhost:8001';
+
+router.get('/', async function(req, res) {
+    console.log(`${configUrl}/cards`);
+    rp(`${configUrl}/cards`)
+        .then(JSON.parse)
+        .then(res.status(200).jsonp.bind(res))
+        .catch(console.error);
+});
+
+//router.get('/:cardname', function(req, res) {
+//    //let cardname = req.params.cardname;
+//    //let card = cards.find(c => c.name === cardname);
+//    //res.jsonp(card.cardData);
+//    console.log(`/${req.params.cardname}`)
+//});
+
+router.get('/:cardname/extremes', async function(req, res) {
+    console.log(`${dataUrl}/${req.params.cardname}/extremes`);
+    rp({uri: `${dataUrl}/${req.params.cardname}/extremes`, simple: false})
+        .then(JSON.parse)
+        .then(res.status(200).jsonp.bind(res))
+        .catch(console.error);
+});
+
+router.get('/:cardname/data', async function(req, res) {
+    console.log(`${dataUrl}/${req.params.cardname}/data`);
+    rp({uri: `${dataUrl}/${req.params.cardname}/data`, qs: req.query, simple: false})
+        .then(JSON.parse)
+        .then(res.status(200).jsonp.bind(res))
+        .catch(console.error);
+});
+
+
+module.exports = router;
